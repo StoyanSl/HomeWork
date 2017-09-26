83d8c7b892541c500e4caaaaf8205ea232058d62
Stoyan Slavchev <Stoyansl29@gmail.com>
2017-09-26 23:51:01 +0300

    CS Intro and Basic Syntax - Lab (First Upload)

---
+++ CS Intro and Basic Syntax - Lab/Add Two Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number0 = int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", number0, number1, number0 + number1);
+        }
+    }
+}
---
+++ CS Intro and Basic Syntax - Lab/Employee Data/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+            long digits = long.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Employee ID: {0}", digits.ToString("00000000"));
+            Console.WriteLine("Salary: " + $"{salary:f2}");
+
+        }
+    }
+}
